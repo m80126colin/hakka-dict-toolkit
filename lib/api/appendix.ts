c9cd58d971af642cefa7e97ded478e21740bde88
Karinsu <m80126colin@gmail.com>
2018-09-16 11:13:36 +0800

    use querystring module

--- lib/api/appendix.ts
+++ lib/api/appendix.ts
@@ -1,22 +1,28 @@
 import * as _       from 'lodash';
 import * as request from 'request-promise';
+import * as qs      from 'querystring';
 
 import * as filter    from '../filter';
 import * as extracter from '../extracter';
 import * as formatter from '../formatter';
 import { host } from '../util';
-import { WordEntry, CharacterEntry } from '../formatter/_type';
+import { ExtractData } from '../extracter/_type';
 
-const appendix = (idx : number) : Promise<WordEntry | CharacterEntry> => {
-  const options = {
-    method: 'GET',
-    uri: `${host}/result_detail.jsp?n_no=${idx}&soundtype=0&sample=[`
-  }
-  return request(options)
+const appendix = (id : string, type : number) : Promise<ExtractData[][]> => {
+  const query = qs.stringify({
+    idno:      id,
+    soundtype: 0,
+    soundnum:  type
+  })
+  return request({
+      method: 'GET',
+      uri: `${host}/Showkou345.jsp?${query}`,
+      strictSSL: false
+    })
     .then(filter.image)
     .then(filter.character)
-    .then(extracter.entry)
-    .then(formatter.entry)
+    .then(extracter.appendix)
+    // .then(formatter.entry)
 }
 
 export default appendix
--- lib/api/entry.ts
+++ lib/api/entry.ts
@@ -1,10 +1,11 @@
 import * as _       from 'lodash';
 import * as request from 'request-promise';
+import * as qs      from 'querystring';
 
 import * as filter    from '../filter';
 import * as extracter from '../extracter';
 import * as formatter from '../formatter';
-import { host, randomHan } from '../util';
+import { host, random } from '../util';
 import { WordEntry, CharacterEntry } from '../formatter/_type';
 /**
  * Provide access to the entry of MOE Hakka Dictionary by index and returns a promise.
@@ -13,15 +14,19 @@ import { WordEntry, CharacterEntry } from '../formatter/_type';
  * @returns a promise with either a word or a character
  */
 const entry = (idx : number) : Promise<WordEntry | CharacterEntry> => {
-  const options = {
-    method: 'GET',
-    uri: `${host}/result_detail.jsp?n_no=${idx}&soundtype=0&sample=${encodeURI(randomHan())}`,
-    strictSSL: false
-  }
-  return request(options)
+  const query = qs.stringify({
+    n_no: idx,
+    soundtype: 0,
+    sample: random.sinitic()
+  })
+  return request({
+      method: 'GET',
+      uri: `${host}/result_detail.jsp?${query}`,
+      strictSSL: false
+    })
     .then(filter.image)
     .then(filter.character)
-    .then(extracter.entry) 
+    .then(extracter.entry)
     .then(formatter.entry)
 }
 
